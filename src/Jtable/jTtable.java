a3a8b0a3f7b2f856097aeefbc4c9bec244a5d024
Minh Chien <chiennmd00419@fpt.edu.vn>
2017-09-29 22:09:56 +0700

    bai tap ve nha va kiem tra

--- src/Jtable/jTtable.java
+++ src/Jtable/jTtable.java
@@ -5,40 +5,34 @@
  */
 package Jtable;
 
-
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-
- 
 /**
  *
  * @author TUNGDUONG
  */
-public class jTtable extends JFrame{
- 
+public class jTtable extends JFrame {
+
     public jTtable() {
- 
+
         String[] titles = new String[]{"id", "name", "classname", "old"};
- 
+
         Object[][] objects = new Object[][]{
             {1, "A", " dvd", 22},
             {2, "B ", " dvdvd", 15},
             {3, "C ", " dvdv", 7}};
-        
         JTable table = new JTable(objects, titles);
-        
         this.add(new JScrollPane(table));
-        this.setTitle("Kênh Lập Trình - Hướng dẫn lập trình Web");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
+        this.setTitle("bang danh sach");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
     }
-    
+
     public static void main(String[] args) {
         new jTtable().setVisible(true);
     }
- 
-}
 
+}
